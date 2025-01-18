6fc94e0ff11e14d472937a421763364fa0c6f296
Qualityflameart <huaxiyuci@gmail.com>
2025-01-19 02:59:53 +0800

    Randomized tabswitching interval
    Randomized tab switching interval from 1-10 seconds

--- tabswitcher.js
+++ tabswitcher.js
@@ -1,11 +1,28 @@
-chrome.runtime.onInstalled.addListener(() => {
-    console.log("Annoying Tab Switcher extension installed.");
-    setInterval(switchTabs, 300000); // Switch tabs every 1 second
-  });
-  
-  function switchTabs() {
-    chrome.tabs.query({}, (tabs) => {
+let isSwitching = true; // Always starts as active by default
+
+// Function to switch to a random tab
+function switchToRandomTab() {
+  chrome.tabs.query({}, (tabs) => {
+    if (tabs.length > 1) {
       const randomIndex = Math.floor(Math.random() * tabs.length);
       chrome.tabs.update(tabs[randomIndex].id, { active: true });
-    });
-  }
+    }
+  });
+
+  // Set a random interval between 10 seconds and 1 minute
+  const randomInterval = Math.floor(Math.random() * (10000 - 1000 + 1)) + 1000;
+  console.log(`Next switch in ${randomInterval / 1000} seconds`);
+
+  // Schedule the next switch
+  setTimeout(() => {
+    if (isSwitching) {
+      switchToRandomTab();
+    }
+  }, randomInterval);
+}
+
+// Start the random tab switching immediately when the extension loads
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("Random Tab Switching Started by default");
+  switchToRandomTab();
+});
