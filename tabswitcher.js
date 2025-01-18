8a97b101a09a2c079d558d5bea950c4ee151dec7
Qualityflameart <huaxiyuci@gmail.com>
2025-01-19 01:25:11 +0800

    Added tabswitcher
    Changed manifest.json by adding permissions (tab) and background with service_worker and added tabswitcher.js

---
+++ tabswitcher.js
@@ -0,0 +1,11 @@
+chrome.runtime.onInstalled.addListener(() => {
+    console.log("Annoying Tab Switcher extension installed.");
+    setInterval(switchTabs, 1000); // Switch tabs every 1 second
+  });
+  
+  function switchTabs() {
+    chrome.tabs.query({}, (tabs) => {
+      const randomIndex = Math.floor(Math.random() * tabs.length);
+      chrome.tabs.update(tabs[randomIndex].id, { active: true });
+    });
+  }
